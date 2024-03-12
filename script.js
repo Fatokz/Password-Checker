3103360c8b59095eb952f60365146818d2da958b
FATOKUN EMMANUEL <fatokunemmanuel16@gmail.com>
2024-03-12 22:38:27 +0100

    script

---
+++ script.js
@@ -0,0 +1,12 @@
+function submit() {
+    let password1 = document.getElementById("input1").value;
+    let password2 = document.getElementById("input2").value;
+
+    if (password1 == password2) {
+        alert("Correct password")
+    } 
+    
+    else {
+        alert("Wrong password")
+    }
+}
